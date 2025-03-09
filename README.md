103acf317f3dc48d2bb4ffcca1fc52e2187592a3
conradqq <connorhawkes@gmail.com>
2025-03-09 10:44:25 -0500

    Added better comments and improved README

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # mkvupdate
-Script to automatically update mkvs
+Command line tool that programatically modifies the resolution of MKV files within a given directory.
+
+Dependencies: 
+
+python3
+MKVToolNix
+
+USAGE EXAMPLE:
+
+Navigate to where mkvUpdate.py is located
+
+"python3 mkvUpdate.py /path/to/mkv/folder width height"
--- mkvUpdate.py
+++ mkvUpdate.py
@@ -4,9 +4,13 @@ import sys
 
 def modify_mkv_resolution(dir, width, height):
   
+  # Select and Loop over MKVs in a directory
+  
   for filename in os.listdir(dir):
     if filename.lower().endswith(".mkv"):
       filepath = os.path.join(dir, filename)
+      
+      # Edit the display width value on the MKV
       
       try: 
         command_width = [
@@ -17,6 +21,8 @@ def modify_mkv_resolution(dir, width, height):
         ]
         subprocess.run(command_width, check=True)
         
+        # Edit the display height value on the MKV
+        
         command_height = [
           "mkvpropedit",
           filepath,
@@ -25,12 +31,14 @@ def modify_mkv_resolution(dir, width, height):
         ]
         subprocess.run(command_height, check=True)
         
+        # Error checking
+        
       except subprocess.CalledProcessError as e:
         print(f"  Error modifying resolution: {e}")
       except Exception as e:
         print(f"  An unexpected error occurred: {e}")
         
-      
+# Uses sys module to add function arguments to variables, then calls modify_mkv_resoultion
 if len(sys.argv) == 4:
   
   directory_path = sys.argv[1]
@@ -38,6 +46,7 @@ if len(sys.argv) == 4:
   width_val = sys.argv[3]
   modify_mkv_resolution(directory_path, height_val, width_val)
   
+# Error checking for bad CLI usage
 else:
     print("Usage: python3 mkvUpdate.py <directory> <height> <width>")
 
